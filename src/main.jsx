478d481ea17bd2d73aa0ae29d8fee4f867131030
Marlusa <marluswm7@gmail.com>
2024-10-31 22:58:52 -0300

    Project Init

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client' //exportando pelo export NomeDaCom
 import Details from './pages/Details/Details'
 import  { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import GlobalStyles from './styles/global';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Details />
     </ThemeProvider>
   </StrictMode>,
--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,10 +1,13 @@
 import { Container } from "./styles.js";
+import { Button } from "../../components/button/index.jsx";
 
 function Details() {
 
      return (
           <Container>
                <h1>Hello World!</h1>
+               <Button title="Entrar" loading/>
+               <Button title="Salvar"/>
                <span>Regra do ReactJS</span>
           </Container>
      )
